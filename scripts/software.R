febbb4ad5c4b082adb1cc6b7c98cf70735f9466d
earowang <earo.wang@gmail.com>
2019-09-18 14:51:34 +1000

    lock downloads to submit dates

--- scripts/software.R
+++ scripts/software.R
@@ -1,22 +1,13 @@
 ## ---- software-impact
-library(gh)
 library(cranlogs)
 library(tidyverse)
 library(lubridate)
 
 my_pkgs <- c("sugrrants", "tsibble", "mists")
 
-## ---- software-stars
-sugrrants_stars <- gh("/repos/:owner/:repo",
-  owner = "earowang", repo = my_pkgs[1])$stargazers_count
-tsibble_stars <- gh("/repos/:owner/:repo",
-  owner = "tidyverts", repo = my_pkgs[2])$stargazers_count
-mists_stars <- gh("/repos/:owner/:repo",
-  owner = "earowang", repo = my_pkgs[3])$stargazers_count
-
 ## ---- software-downloads
-sugrrants_dl <- cran_downloads(my_pkgs[1], from = "2017-07-28")
-tsibble_dl <- cran_downloads(my_pkgs[2], from = "2018-01-09")
+sugrrants_dl <- cran_downloads(my_pkgs[1], from = "2017-07-28", to = "2019-09-20")
+tsibble_dl <- cran_downloads(my_pkgs[2], from = "2018-01-09", to = "2019-09-20")
 df_dl <- bind_rows(sugrrants_dl, tsibble_dl)
 df_dl %>% 
   ggplot(aes(x = date, y = count)) +
