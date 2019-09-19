f668e4e48eaa1bbe8e96d82d7e96010f717dbc4e
earowang <earo.wang@gmail.com>
2019-09-19 18:05:43 +1000

    fig and bib all good #5

--- scripts/tsibble-smart-meter.R
+++ scripts/tsibble-smart-meter.R
@@ -7,13 +7,13 @@ library(patchwork)
 theme_set(theme_bw())
 
 elec <- read_rds("data/tsibble/smart-meter13.rds")
-weather <- read_rds("data/tsibble/weather13.rds") %>% 
-  group_by(date) %>% 
-  mutate(avg_temp = mean(c(max_temp, min_temp))) %>% 
-  ungroup() %>% 
+weather <- read_rds("data/tsibble/weather13.rds") %>%
+  group_by(date) %>%
+  mutate(avg_temp = mean(c(max_temp, min_temp))) %>%
+  ungroup() %>%
   mutate(hot = if_else(avg_temp > 25, "Hot day", "Not hot day"))
 
-elec_ts <- elec %>% 
+elec_ts <- elec %>%
   build_tsibble(
     key = customer_id, index = reading_datetime,
     validate = FALSE, ordered = TRUE
@@ -24,17 +24,17 @@ gap_df <- has_gaps(elec_ts)
 # sum(gap_df$.gaps) / NROW(gap_df)
 
 ## ---- count-gaps
-count_na_df <- elec_ts %>% 
+count_na_df <- elec_ts %>%
   count_gaps()
 
-lumped_na_df <- count_na_df %>% 
+lumped_na_df <- count_na_df %>%
   mutate(
-    customer_id = as.factor(customer_id) %>% 
+    customer_id = as.factor(customer_id) %>%
       fct_lump(50) %>% fct_reorder(.n, sum)
-  ) 
+  )
 
-p_49 <- lumped_na_df %>% 
-  filter(customer_id != "Other") %>% 
+p_49 <- lumped_na_df %>%
+  filter(customer_id != "Other") %>%
   ggplot(aes(x = customer_id)) +
   geom_linerange(aes(ymin = .from, ymax = .to)) +
   geom_point(aes(y = .from), size = 0.6, shape = 4) +
@@ -49,8 +49,8 @@ p_49 <- lumped_na_df %>%
     plot.margin = unit(c(0, 0, -1, -1), "line")
   )
 
-p_other <- lumped_na_df %>% 
-  filter(customer_id == "Other") %>% 
+p_other <- lumped_na_df %>%
+  filter(customer_id == "Other") %>%
   ggplot(aes(x = customer_id)) +
   geom_linerange(aes(ymin = .from, ymax = .to), alpha = 0.1) +
   geom_point(aes(y = .from), size = 1.2, shape = 4, alpha = 0.1) +
@@ -69,18 +69,18 @@ p_49 + p_other + plot_layout(ncol = 1, heights = c(10, 1))
 
 ## ---- calendar-plot
 library(sugrrants)
-elec_full <- elec_ts %>% 
-  summarise(avg = mean(general_supply_kwh)) %>% 
+elec_full <- elec_ts %>%
+  summarise(avg = mean(general_supply_kwh)) %>%
   mutate(
-    date = as_date(reading_datetime), 
+    date = as_date(reading_datetime),
     time = hms::as_hms(reading_datetime)
-  ) %>% 
+  ) %>%
   left_join(weather, by = "date")
 
-elec_cal <- elec_full %>% 
+elec_cal <- elec_full %>%
   frame_calendar(x = time, y = avg, date = date)
 
-p_cal <- elec_cal %>% 
+p_cal <- elec_cal %>%
   ggplot(aes(.time, .avg, group = date, colour = hot)) +
   geom_line() +
   scale_colour_brewer(palette = "Dark2", direction = -1, name = "") +
@@ -88,20 +88,20 @@ p_cal <- elec_cal %>%
 prettify(p_cal)
 
 ## ---- elec-model
-elec_train <- elec_full %>% 
+elec_train <- elec_full %>%
   filter_index("2013-12-01" ~ "2013-12-30")
-elec_test <- elec_full %>% 
+elec_test <- elec_full %>%
   filter_index("2013-12-31" ~ .)
 
-elec_fc <- elec_train %>% 
+elec_fc <- elec_train %>%
   model(
     `temperature` = ARIMA(log(avg) ~ avg_temp),
     `w/o temperature` = ARIMA(log(avg))
-  ) %>% 
+  ) %>%
   forecast(new_data = elec_test)
 
 ## ---- elec-forecast
-elec_fc %>% 
+elec_fc %>%
   autoplot(data = filter_index(elec_train, "2013-12-19" ~ .), level = NULL) +
   geom_line(aes(y = avg), data = elec_test, linetype = "dashed") +
   scale_colour_brewer(palette = "Dark2", direction = -1, name = "") +
@@ -111,8 +111,8 @@ elec_fc %>%
   ylab("Average electricity use")
 
 ## ---- elec-accuracy
-accuracy(elec_fc, elec_test) %>% 
-  rename(model = .model) %>% 
-  select(-.type, -MASE, -ACF1) %>% 
+accuracy(elec_fc, elec_test) %>%
+  rename(model = .model) %>%
+  select(-.type, -MASE, -ACF1) %>%
   knitr::kable(digits = 3, booktabs = TRUE, caption = "(ref:elec-accuracy)", linesep = "") %>%
   kableExtra::kable_styling(position = "center", latex_options= "hold_position")
